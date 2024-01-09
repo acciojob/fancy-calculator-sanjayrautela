9555618f0c11b6a22a914e8edbf54d1f0511c6e2
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-09 23:01:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //your JS code here. If required.
-let firstInput = parseInt(prompt("Enter the first number"));
-let multipliedResult = firstInput * 2;
+// let firstInput = parseInt(prompt("Enter the first number"));
+// let multipliedResult = firstInput * 2;
 
-let secondInput = parseInt(prompt("Enter the second number"));
+// let secondInput = parseInt(prompt("Enter the second number"));
 
-let finalResult = multipliedResult + secondInput;
+// let finalResult = multipliedResult + secondInput;
 
-alert("the final result is:" + finalResult);
+// alert("the final result is:" + finalResult);
+let x = prompt("Enter first number:");
+let y = prompt("Enter second number:");
+alert(`The final result is: ${parseInt(x) * 2 + parseInt(y)}`);
+
